2d5585c7719c3570c03fb006bcc01ab67b19ca31
rajibdas-it <rajibrad@gmail.com>
2022-10-25 00:38:07 +0600

    api created for all course

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("study elegant server running");
+});
+
+app.get("/allcourses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
